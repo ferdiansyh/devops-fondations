73549dbd8aa43cbc1b6398f088d7e69ae78e48da
ferdiansyh <ferdiancex@gmail.com>
2025-12-23 17:22:59 +0700

    menambah script automasi cek sistem

---
+++ system_check.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "--- Laporan Kesehatan Sistem---"
+echo "Waktu sekarang:"
+date
+echo ""
+
+echo " user yang sedang aktif:"
+whoami
+echo ""
+
+echo "status ruang penyimpanan (disk):"
+df -h
+echo "-----""
